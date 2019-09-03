2da50f5f68a4a7479355cbfa315c82dae48b9062
lllJVSlll <johan.simonsen@elev.ga.ntig.se>
2019-09-03 11:58:11 +0200

    skapat lllJVSlll
    yes

---
+++ src/simonsen.java
@@ -0,0 +1,10 @@
+
+public class simonsen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("scraeM and pancek men adventure pls drop me mFOE");
+	}
+
+}
