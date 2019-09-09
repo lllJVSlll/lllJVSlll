331256eebca29c8b0f72e5a54123d6d451ff5893
lllJVSlll <johan.simonsen@elev.ga.ntig.se>
2019-09-09 13:08:22 +0200

    yessir

---
+++ V37/v37/lektion1.java
@@ -0,0 +1,30 @@
+package v37;
+
+public class lektion1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*ett annat sätt att skriva kommentarer
+		 */
+
+		int heltal1 = 5; // skapar en låda som heter heltal1 och fyller den med ett heltal
+		int svar; // deklarera
+		
+		svar = heltal1*heltal1;
+		
+		System.out.println("svar: " + heltal1*heltal1); // + tecknet kallas konkatinera
+		
+		double pi = 3.14;
+		double radie = 5;
+		
+		double area = pi*radie*radie;
+				
+				System.out.println("arean är : " + area + " cm^2");
+				
+				int stortHeltal= 5000;
+				
+				System.out.println(stortHeltal +1);
+		
+	}
+
+}
---
+++ V37/v37/provaSjalva.java
@@ -0,0 +1,10 @@
+package v37;
+
+public class provaSjalva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
