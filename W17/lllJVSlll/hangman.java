ea3bc73dd5818701c2c2c1434015d952d6b479da
lllJVSlll <johan.simonsen@elev.ga.ntig.se>
2020-05-08 23:21:53 +0200

    klar hänga gubbe

---
+++ W17/lllJVSlll/hangman.java
@@ -0,0 +1,15 @@
+package lllJVSlll;
+
+import java.util.Scanner;
+
+public class hangman {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Välkommen till Hänga gubbe!");
+		syso
+
+	}
+
+}
